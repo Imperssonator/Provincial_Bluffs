5a78b459de54666dda450c1c657d6ce20e301fe9
Nils <imperssonator@gmail.com>
2015-07-07 13:34:39 -0400

    Tried different parameters for 4th peak

--- PlotEB.m
+++ PlotEB.m
@@ -1,10 +1,22 @@
 function [] = PlotEB(StructPath)
 
 load(StructPath)
-figure
-hold on
 
-for i = 4:length(UVS)
-    plot(i,UVS(i).EB,'ob')
+EB = zeros(length(UVS),1);
+MSE = zeros(length(UVS),1);
+
+EB = [UVS(:).EB]';
+MSE = [];
+for i = 1:length(UVS)
+    MSE(i,1) = UVS(i).GMM.MSE;
 end
+
+figure
+subplot(2,1,1), plot((1:length(UVS)),EB,'ob')
+title('Exciton Bandwidth')
+xlabel('Run')
+ylabel('Exciton Bandwidth (eV)')
+subplot(2,1,2), plot((1:length(UVS)),MSE,'*r')
+title('Mean Square Error of Gaussian Fits')
+
 end
